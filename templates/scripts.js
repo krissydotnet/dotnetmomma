a50cd1262d92fcc032e44083227425183b7784a7
Krissy Dukes <krissydotnet@yahoo.com>
2018-10-25 13:55:30 -0400

    Fix toggle for navigation bar
    Fix toggle for navigation bar

--- templates/scripts.js
+++ templates/scripts.js
@@ -2,3 +2,9 @@ $(function(){
     $("#header").load("./templates/header.html"); 
     //$("#footer").load("footer.html"); 
   });
+  function classToggle() {
+    document.querySelectorAll('.navbar-items').forEach(nav => nav.classList.toggle('navbar-toggleshow'));
+  }
+  // document.querySelector('navbar-toggle')
+  // .addEventListener('click', classToggle);
+
